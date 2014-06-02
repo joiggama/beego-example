a6097da2919c9704518e3376280ba76bb481a45a
Ignacio Galindo <joiggama@gmail.com>
2014-06-02 18:57:32 -0500

    Use DATABASE_URL

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
-	_ "github.com/joiggama/beego-example/routers"
+  "os"
+
 	"github.com/astaxie/beego"
+
   _ "github.com/lib/pq"
   "github.com/astaxie/beego/orm"
+
+	_ "github.com/joiggama/beego-example/routers"
+
+  "github.com/codegangsta/envy/lib"
 )
 
 func init() {
+  envy.Bootstrap()
+
   orm.RegisterDriver("postgres", orm.DR_Postgres)
-  orm.RegisterDataBase("default", "postgres", "dbname=example_app_dev sslmode=disable")
+  orm.RegisterDataBase("default", "postgres", os.Getenv("DATABASE_URL"))
 }
 
 func main() {
