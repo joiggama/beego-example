de30a566bb6479211ce40e6586921022cc750fad
Ignacio Galindo <joiggama@gmail.com>
2014-06-02 18:59:23 -0500

    Add Procfile

--- main.go
+++ main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-  "os"
+	"os"
+	"strconv"
 
 	"github.com/astaxie/beego"
 
-  _ "github.com/lib/pq"
-  "github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
 
 	_ "github.com/joiggama/beego-example/routers"
 
-  "github.com/codegangsta/envy/lib"
+	"github.com/codegangsta/envy/lib"
 )
 
 func init() {
-  envy.Bootstrap()
+	envy.Bootstrap()
 
-  orm.RegisterDriver("postgres", orm.DR_Postgres)
-  orm.RegisterDataBase("default", "postgres", os.Getenv("DATABASE_URL"))
+	orm.RegisterDriver("postgres", orm.DR_Postgres)
+	orm.RegisterDataBase("default", "postgres", os.Getenv("DATABASE_URL"))
 }
 
 func main() {
+	port, err := strconv.Atoi(os.Getenv("PORT"))
+	if err == nil {
+		beego.HttpPort = port
+	}
 	beego.Run()
 }
-
